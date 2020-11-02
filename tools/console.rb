271a8c43c6e24b59cfa37e7648d9dd004d15d846
Derick Castillo <derick.castillo@flatironschool.com>
2020-11-02 16:25:12 -0600

    Update Readme

--- tools/console.rb
+++ tools/console.rb
@@ -7,8 +7,6 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-max = Baller.new("Max")
-derick = Baller.new("Derick")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
